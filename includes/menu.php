d83dacaaf18faa6c8c659f50a6f0a59b55f90208
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-07-23 06:56:25 -0300

    feat: add href on logo

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,9 @@
 <div class="col-3 container-menu h-100">
     <div class="w-100">
-        <img src="../img/logo.png" alt="Logo optimanager" class="w-100 h-100" style="-webkit-filter: drop-shadow(0px 0px 20px rgba(255 255 255 / 0.05)); 
-        filter: drop-shadow(0px 0px 20px rgba(255 255 255 / 0.08));">
+        <a href="../home" class="link-reset">
+            <img src="../img/logo.png" alt="Logo optimanager" class="w-100 h-100" style="-webkit-filter: drop-shadow(0px 0px 20px rgba(255 255 255 / 0.05)); 
+            filter: drop-shadow(0px 0px 20px rgba(255 255 255 / 0.08));">
+        </a>
     </div>
     <nav class="nav flex-column p-2 pb-5 mt-2 gap-3" style="color: white;">
         <a class="nav-link fs-4 text-reset fw-light" href="../home">Home</a>
